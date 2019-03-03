7aadf135ed8a9b623e1eeee1acf5a79274d6335a
skyyyx <thepro1999.vl@gmail.com>
2019-03-04 08:57:36 +0900

    task1

--- wc-l/wc-l.cpp
+++ wc-l/wc-l.cpp
@@ -7,16 +7,18 @@
 #include <windows.h>
 #include <fstream>
 
+
 using namespace std;
 
 int main(int argc, char** argv ) {
     long long t1, t2, freq;
 	string str;
+	int kol=0;
 	
 	//if (argc == 1) 
     //{
-    //    cerr << "Error: need text file\n";
-    //    return 1;
+     //   cerr << "Error: need text file\n";
+      //  return 1;
     //}
 	
 	cout << "Opening file a.txt\n"; // << argv[1] << "\n";
@@ -27,9 +29,16 @@ int main(int argc, char** argv ) {
 
 
 	QueryPerformanceCounter((LARGE_INTEGER *)&t1);// смотрим время после окончания цикла
+	//for(int i=0; i<=4; i++)
+	while(f.eof()!=true)
+	{
+
 	getline(f, str);
+	kol++;
+	}
 	QueryPerformanceCounter((LARGE_INTEGER *)&t2);// смотрим время после окончания цикла
 
 	cout << str << "\n Time spent:" << (t2-t1)/(1.*freq);
+	cout<<"\n "<< kol;
 	return 0;
 }
