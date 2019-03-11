438889c4e7a780d16e87b8892c4f1613b4947cee
Olimp666 <abrakadaniel@mail.ru>
2019-03-11 09:32:54 +0900

    Task4

--- wc-l/wc-l.cpp
+++ wc-l/wc-l.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main(int argc, char** argv ) {
     long long t1, t2, freq;
 	string str;
-	int kol=0;
+	int kol = 0;
 	
 	//if (argc == 1) 
     //{
@@ -22,22 +22,34 @@ int main(int argc, char** argv ) {
     //}
 	
 	cout << "Opening file a.txt\n"; // << argv[1] << "\n";
+	char * s = new char[3000];
+	//ifstream f("a.txt");
+	FILE *f;
+
+	f = fopen("a.txt", "r");
 	
-	ifstream f("a.txt");
+	
+	
 	
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq);// запрашиваем число тиков в 1 сек
 
-
 	QueryPerformanceCounter((LARGE_INTEGER *)&t1);// смотрим время после окончания цикла
+	while(!feof(f)){
+	fgets(s, 3000, f);
+	kol++;
+	}
+	
+	
+	
 	//for(int i=0; i<=4; i++)
-	while(f.eof()!=true)
+	/*while(!f.eof())
 	{
 
 	getline(f, str);
 	kol++;
-	}
+	}*/
 	QueryPerformanceCounter((LARGE_INTEGER *)&t2);// смотрим время после окончания цикла
-
+	fclose(f);
 	cout << str << "\n Time spent:" << (t2-t1)/(1.*freq);
 	cout<<"\n "<< kol;
 	return 0;
